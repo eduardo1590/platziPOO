174db15b763f8264e62736951ee3c921e5ab261c
Eduardo ARG <eduardo1590@gmail.com>
2020-09-15 17:07:17 -0400

    herencia en PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,14 @@
 <?php
 require_once("Car.php");
 require_once("Account.php");
+require_once("uberX.php");
+require_once("uberPool.php");
 
-$car = new Car("AMQ123", new Account("Andres Herrera", "V20123456"));
-$car->printDataCar();
+/*$car = new Car("AMQ123", new Account("Andres Herrera", "V20123456"));
+$car->printDataCar();*/
+
+$uberX = new UberX("AMQ123", new Account("Andres Herrera", "V20123456"), "Toyota", "Corolla");
+$uberX->printDataCar();
+
+$uberPool =  new UberPool("AMQ456", new Account("Andrea Herrera", "V2023456"), "Nissan", "Sentra");
+$uberPool->printDataCar();
