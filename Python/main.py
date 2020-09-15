a46a5abdafb881abb1804e43a52303761de87623
Eduardo ARG <eduardo1590@gmail.com>
2020-09-15 15:40:37 -0400

    constructor en python

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,8 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMQ123"
-    car.driver = "Andres Herrera"
+    car = Car("AMQ123", Account("Andres Herrera", "V20123456"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Andrea Herrera"
-    print(vars(car2))
+    print(vars(car.driver))
