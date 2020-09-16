1a36627285c80e0d8ab693bd6777dd97632056a1
Eduardo ARG <eduardo1590@gmail.com>
2020-09-15 20:22:05 -0400

    instanciando distintos objetos

--- Java/Main.java
+++ Java/Main.java
@@ -2,11 +2,15 @@ class Main{
     public static void main(String[] args) {
         
         Car car = new Car("AMQ123", new Account("Andres Herrera", "V20123456"));
-        car.passenger = 4;
+        car.setPassenger(4);
         car.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "V20234567"));
-        car2.passenger = 3;
-        car2.printDataCar();
+        UberX uberX = new UberX("QWE567", new Account("Andrea Herrera", "V20234567"), "Toyota", "Corolla");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+
+        UberVan uberVan = new UberVan("QWE123", new Account("Andy Herrera", "V20345678"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
     }
 }
